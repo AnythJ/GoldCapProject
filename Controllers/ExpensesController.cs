ec09af005c63e0918af9cb6ee240cd45a588b1ef
AnythJ <jkb.czelusta@gmail.com>
2021-07-16 17:34:27 +0200

    Added action for create and edit ("CreateOrEdit). Added modals for both actions. Added validations. Added Delete action and confirmation alert.

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,78 @@
+﻿using GoldCap.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static GoldCap.Helper;
+
+namespace GoldCap.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private IExpenseRepository _expenseRepository;
+
+        public ExpensesController(IExpenseRepository expenseRepository)
+        {
+            _expenseRepository = expenseRepository;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Expenses = _expenseRepository;
+            var expenses = _expenseRepository.GetAllExpenses();
+
+            return View(expenses);
+        }
+
+        // GET: Expenses/CreateOrEdit
+        // GET: Expenses/CreateOrEdit/id
+        [HttpGet]
+        [NoDirectAccess]
+        public IActionResult CreateOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Expense());
+            else
+            {
+                var expenseModel = _expenseRepository.GetExpense(id);
+                if (expenseModel == null)
+                {
+                    return NotFound();
+                }
+                return View(expenseModel);
+            }
+        }
+
+        // POST: Expenses/CreateOrEdit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateOrEdit(int id, [Bind("Id,Amount,Category,Description,Date")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == 0)
+                {
+                    _expenseRepository.Add(expense);
+                }
+                else
+                {
+                    _expenseRepository.Update(expense);
+                }
+
+
+                return Json(new { isValid = true, html = Helper.RenderRazorViewToString(this, "_ViewAll", _expenseRepository.GetAllExpenses()) });
+            }
+            return Json(new { isValid = false, html = Helper.RenderRazorViewToString(this, "CreateOrEdit", expense) });
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _expenseRepository.Delete(id);
+
+            return Json(new { html = Helper.RenderRazorViewToString(this, "_ViewAll", _expenseRepository.GetAllExpenses()) });
+        }
+    }
+}
--- Models/Expense.cs
+++ Models/Expense.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,18 @@ namespace GoldCap.Models
 {
     public class Expense
     {
+        [Key]
         public int Id { get; set; }
+        [DisplayName("Amount")]
+        [Required(ErrorMessage = "Invalid")]
         public decimal Amount { get; set; }
+        [DisplayName("Category")]
         public Ctg Category { get; set; }
+        [StringLength(135)]
+        [DisplayName("Description")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Invalid")]
+        [DisplayName("Date")]
         public DateTime Date { get; set; }
     }
 }

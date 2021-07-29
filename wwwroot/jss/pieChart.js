229decbe4d298d9cde30f80a520f33a086db4e6d
AnythJ <jkb.czelusta@gmail.com>
2021-07-29 12:57:34 +0200

    Changed styles, added scrollbar in dashboard but screwed styling for other elements.

---
+++ wwwroot/jss/pieChart.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+    $.getJSON("Dashboard/GetData", function (result) {
+        var cateData = [];
+        $.each(result.categoryRatios, function (index, element) {
+            cateData.push({
+                name: element.categoryName,
+                y: parseFloat(element.categoryPercentage),
+            });
+        }),
+            Highcharts.chart('container-pie', {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'Categories in %'
+                },
+                exporting: {
+                    enabled: false
+                },
+                //tooltip: {
+                //    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                //},
+                accessibility: {
+                    point: {
+                        valueSuffix: '%'
+                    }
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                        }
+                    }
+                },
+                series: [{
+                    name: '',
+                    colorByPoint: true,
+                    data: cateData
+                }]
+            });
+    });
+});

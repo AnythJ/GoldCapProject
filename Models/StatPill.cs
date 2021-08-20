f263d1ce6fe08f52d5a8d518632ec6e89a35ba10
AnythJ <jkb.czelusta@gmail.com>
2021-08-20 22:06:30 +0200

    Added StatPill model. Added arrow in second pill.

---
+++ Models/StatPill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldCap.Models
+{
+    public class StatPill : Expense
+    {
+        public int AmountInt { get; set; }
+        public decimal Percentage { get; set; }
+        public string DatetimeString { get; set; }
+    }
+}

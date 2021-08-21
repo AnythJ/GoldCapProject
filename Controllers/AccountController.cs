365a18cd9af32dc9ae3ac7f08c289a25d476fd80
AnythJ <jkb.czelusta@gmail.com>
2021-08-21 18:12:42 +0200

    Added login option.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using GoldCap.ViewModels;
+﻿using GoldCap.Models;
+using GoldCap.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -55,6 +56,39 @@ namespace GoldCap.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "dashboard");
+                }
+
+                ModelState.AddModelError(string.Empty, "Something went wrong, please try again");
+
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("index", "account");
         }
     }
 }

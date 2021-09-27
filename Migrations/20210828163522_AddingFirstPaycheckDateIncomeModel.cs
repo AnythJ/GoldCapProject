14ccff7faf6f73b12f7bf2be20f05b13e742072f
AnythJ <jkb.czelusta@gmail.com>
2021-09-27 11:00:04 +0200

    Commit before changing device.

---
+++ Migrations/20210828163522_AddingFirstPaycheckDateIncomeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoldCap.Migrations
+{
+    public partial class AddingFirstPaycheckDateIncomeModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FirstPaycheckDate",
+                table: "Incomes",
+                type: "smalldatetime",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstPaycheckDate",
+                table: "Incomes");
+        }
+    }
+}
